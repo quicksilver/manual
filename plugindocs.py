58ddca335d9c0e23f12ae27b0e088d2a0b803d30
Joshua Ringer <joshringer@users.noreply.github.com>
2018-03-29 18:46:16 +0800

    Check qsapp.com plugin directory for supported plugins, drop obsolete ones.

--- plugindocs.py
+++ plugindocs.py
@@ -13,7 +13,7 @@ import plistlib
 import re
 from shutil import copyfile
 from subprocess import run
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 
 import yaml
 
@@ -21,10 +21,39 @@ import yaml
 CACHE_DIR = Path('_plugins')
 CONFIG_PATH = Path(__file__).parent / 'mkdocs.yml'
 DOCS_DIR = Path(__file__).parent / 'docs'
+CHECK_URL = 'https://qs0.qsapp.com/plugins/check.php'
+INFO_URL = 'https://qs0.qsapp.com/plugins/info.php'
 REPOS_URL = 'https://api.github.com/orgs/quicksilver/repos'
 
 
 log = logging.getLogger(__name__)
+
+
+def get_latest_qsversion(check_url=CHECK_URL):
+    """Query for the latest version of Quicksilver."""
+    # User-Agent is used to filter on OS version, so ensure it is not.
+    req = Request(check_url, headers={'User-Agent': 'manual/plugindocs'})
+    log.info('Querying %s for latest qs version', check_url)
+    with urlopen(req) as response:
+        build = response.read().decode()
+
+    return build
+
+
+def get_plugins_info(qsversion=None, info_url=INFO_URL):
+    """Fetch plugin information list from qsapp."""
+    if qsversion:
+        url = f'{info_url}?qsversion={qsversion}'
+    else:
+        url = info_url
+
+    # User-Agent is used to filter on OS version, so ensure it is not.
+    req = Request(url, headers={'User-Agent': 'manual/plugindocs'})
+    log.info('Querying %s for plugins info', url)
+    with urlopen(req) as response:
+        info = plistlib.loads(response.read())
+
+    return info['plugins']
 
 
 def get_plugin_repositories(repos_url=REPOS_URL):
@@ -102,7 +131,7 @@ class Project(object):
         log.debug('Importing plugin %s', source_dir)
         name = source_dir.name[:-len('-qsplugin')]
         fname = name.lower()
-        name = self._parse_info_plist(source_dir) or name
+        name = self._get_plugin_name(source_dir, name)
         dstfile = self.docs_dir / 'plugins' / f'{fname}.md'
         entry = f'plugins/{fname}.md'
         makedirs(dstfile.parent, exist_ok=True)
@@ -113,30 +142,48 @@ class Project(object):
                 log.debug('Adding %s to page index', name)
                 self.pluginstoc[name] = entry
 
-    def _parse_info_plist(self, source_dir):
-        try:
-            with open(source_dir / 'Info.plist', 'rb') as infofp:
-                info = plistlib.load(infofp)
-
-        except OSError:
-            return None
-        else:
-            name = info.get('CFBundleDisplayName')
+    def _get_plugin_name(self, source_dir, default=None):
+        info = self.parse_info_plist(source_dir)
+        name = info.get('CFBundleDisplayName')
+        if not name or name.startswith('$'):
+            name = info.get('CFBundleName')
             if not name or name.startswith('$'):
-                name = info.get('CFBundleName')
-                if not name or name.startswith('$'):
-                    return None
+                return default
 
-            return name
+        return name
+
+    @staticmethod
+    def parse_info_plist(source_dir):
+        """Parse plugin Info.plist."""
+        with open(source_dir / 'Info.plist', 'rb') as infofp:
+            info = plistlib.load(infofp)
+
+        return info
 
 
 def main():
     """Run script."""
+    qsversion = get_latest_qsversion()
+    plugins = get_plugins_info(qsversion)
+    supported_plugin_ids = set()
+    for plugin in plugins:
+        plugin_id = plugin.get('CFBundleIdentifier')
+        if plugin_id:
+            supported_plugin_ids.add(plugin_id)
+
     project = Project()
     for repo in get_plugin_repositories():
-        source_dir = cache_code(repo['full_name'], repo['clone_url'])
-    # for source_dir in Path('_plugins/quicksilver').iterdir():
-        project.import_plugin_doc(source_dir)
+        try:
+            source_dir = cache_code(repo['full_name'], repo['clone_url'])
+            pid = project.parse_info_plist(source_dir).get('CFBundleIdentifier')
+            if pid not in supported_plugin_ids:
+                log.warning('Ignoring obsolete plugin: %s', pid)
+                continue
+
+            project.import_plugin_doc(source_dir)
+        except Exception as exc:
+            log.warning('Error importing %s: %s', repo.get('full_name'), exc)
+            log.debug('Exception', exc_info=True)
 
     project.save()
 
